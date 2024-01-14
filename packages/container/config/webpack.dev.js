d64729bcb44662d525e9e3dd49c25841c95a3020
Antonio Gil <agil@fmit-solutions.com>
2024-01-14 16:45:49 +0100

    chore(container): add production webpack config

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,7 +1,7 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
 const devConfig = {
   mode: 'development',
@@ -17,9 +17,7 @@ const devConfig = {
       remotes: {
         marketing: 'marketing@http://localhost:8081/remoteEntry.js',
       },
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
+      shared: packageJson.dependencies,
     }),
   ],
 };

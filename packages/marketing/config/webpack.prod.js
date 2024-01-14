9730871f2db887a9fe3fc5614d8c07ef19d7821e
Antonio Gil <agil@fmit-solutions.com>
2024-01-14 16:49:11 +0100

    chore(marketing): add production webpack config

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -1,12 +1,21 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
 
 const prodConfig = {
   mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+  },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap',
+      },
+      shared: packageJson.dependencies,
     }),
   ],
 };

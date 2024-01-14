4164816080c2f4a7c893fc7531b0dddbc1af0321
Antonio Gil <agil@fmit-solutions.com>
2024-01-14 11:25:49 +0100

    feat(container): setup webpack config

---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,14 @@
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const commonConfig = require('./webpack.common');
+
+const prodConfig = {
+  mode: 'production',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
---
+++ packages/container/src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { mount } from 'marketing/MarketingApp';
+
+console.log(mount);
+
+export function App() {
+  return (
+    <>
+      <h1>Hi there!</h1>
+    </>
+  );
+}

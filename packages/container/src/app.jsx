d4662057ffa29c449820eafd6bdb040792449d5f
Antonio Gil <agil@fmit-solutions.com>
2024-01-16 12:28:18 +0100

    chore: improve ci

--- packages/container/src/app.jsx
+++ packages/container/src/app.jsx
@@ -4,7 +4,7 @@ import { MarketingApp } from '../components/marketing-app.jsx';
 export function App() {
   return (
     <>
-      <h1>Hi container!</h1>
+      <h1>Hi there!</h1>
       <hr />
       <MarketingApp />
     </>
